12b913bac25559a63e66fe0b43ed200b57b100b2
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-10-04 10:04:58 +0200

    ProductGroup model

---
+++ LagerStyringProjekt/LSP.models/ProductGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP.models
+{
+    class ProductGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
