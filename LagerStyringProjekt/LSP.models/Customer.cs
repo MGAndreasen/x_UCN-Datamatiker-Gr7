c2ab73fefc32117cbd8e916dfe687047ec5cb7b6
Arnes Kovacevic <1061725@ucn.dk>
2017-10-04 10:04:20 +0200

    aiusdh

---
+++ LagerStyringProjekt/LSP.models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP.models
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Cvr { get; set; }
+        public string Address { get; set; }
+        public int Zip { get; set; }
+       // public CustomerType CustomerType { get; set; }
+
+
+    }
+}
