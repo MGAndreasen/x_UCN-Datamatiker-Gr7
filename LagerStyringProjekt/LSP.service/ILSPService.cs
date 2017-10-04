8f84a094a829835ca4cb01c198cbd69e0a8f5a08
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-10-04 11:05:06 +0200

    test

--- LagerStyringProjekt/LSP.service/ILSPService.cs
+++ LagerStyringProjekt/LSP.service/ILSPService.cs
@@ -7,6 +7,7 @@ using System.Text;
 
 namespace LSP.service
 {
+    // Test
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface ILSPService
