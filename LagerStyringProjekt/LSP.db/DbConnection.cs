08d63acbdbf2aaa6aa101cb9bfac6b59e3e7b7b5
Arnes Kovacevic <1061725@ucn.dk>
2017-10-17 14:08:19 +0200

    Bug Med DBLayer

--- LagerStyringProjekt/LSP.db/DbConnection.cs
+++ LagerStyringProjekt/LSP.db/DbConnection.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace LSP.db
 {
     public class DbConnection
     {
+        private string connectionString;
 
+
+
+
+        public DbConnection()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString; 
+
+        }
 
     }
 }
