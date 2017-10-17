949d44d5fa2ed860b5f91954d5d2517ef4e91121
Arnes Kovacevic <1061725@ucn.dk>
2017-10-17 14:18:05 +0200

    GetAllCustomers

--- LagerStyringProjekt/DbLayer/DbConnection.cs
+++ LagerStyringProjekt/DbLayer/DbConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DbLayer
 {
@@ -17,6 +18,31 @@ namespace DbLayer
         {
             connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
 
+        }
+
+        public List<Customer> getAllAcounts()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(this.connectionString))
+            {
+                List<Customer> acList = new List<Customer>();
+                SqlCommand cmd = new SqlCommand("Select Id, CustomerId, Balance from dbo.Account", sqlCon);
+                sqlCon.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Customer accmeldmig = new Customer();
+                    accmeldmig.AccountId = reader.GetInt32(0);
+                    accmeldmig.CustomerId = reader.GetInt32(1);
+                    accmeldmig.Balance = reader.GetFloat(2);
+                    acList.Add(accmeldmig);
+
+                }
+                return acList;
+
+
+
+            }
         }
     }
 }
