7d4c85454082523aa57a984e82037374c5188dee
Arnes Kovacevic <1061725@ucn.dk>
2017-10-17 14:12:50 +0200

    Ny DBCOnnection

---
+++ LagerStyringProjekt/DbLayer/DbConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DbLayer
+{
+    class DbConnection
+    {
+
+        private string connectionString;
+
+
+        public DbConnection()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+        }
+    }
+}
