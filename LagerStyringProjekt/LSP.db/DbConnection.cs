7a0c9bb3316142dfb8d0ccdc66d95e0d6399ab54
Jacob <Jacob@JACOBG>
2017-10-17 13:59:37 +0200

    heheheeh

--- LagerStyringProjekt/LSP.db/DbConnection.cs
+++ LagerStyringProjekt/LSP.db/DbConnection.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using DatabasesSimpleADO.Models;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LSP.db
 {
     public class DbConnection
     {
-    }
+
+        public void getConnection()
+        {
+            connectionString = ConfigurationManager.ConnectionString["Default"].ConnectionString;
+        }
+                }
 }
